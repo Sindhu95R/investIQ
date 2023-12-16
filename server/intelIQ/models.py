842d3b2780ecec6e66fa1c941dcde06824043cfe
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-12-17 02:13:41 +0300

    Added transaction models

--- server/intelIQ/models.py
+++ server/intelIQ/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    wallet_address = models.CharField(max_length=100)
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cryptocurrency = models.ForeignKey(Cryptocurrency, on_delete=models.CASCADE)
+    transaction_type = models.CharField(max_length=10)  # buy or sell
+    amount = models.DecimalField(max_digits=20, decimal_places=10)
+    price = models.DecimalField(max_digits=20, decimal_places=10)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return super().__str__()
