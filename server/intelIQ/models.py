1bfba6516e39e854f5ccd75f38ba9ec22174776c
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-12-18 01:41:16 +0300

    Added serialization

--- server/intelIQ/models.py
+++ server/intelIQ/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    wallet_address = models.CharField(max_length=100)
+    wallet_address = models.CharField(max_length=100) 
 
 class Transaction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     cryptocurrency = models.ForeignKey(Cryptocurrency, on_delete=models.CASCADE)
-    transaction_type = models.CharField(max_length=10)  # buy or sell
+    transaction_type = models.CharField(max_length=10)  
     amount = models.DecimalField(max_digits=20, decimal_places=10)
     price = models.DecimalField(max_digits=20, decimal_places=10)
     date = models.DateTimeField(auto_now_add=True)
