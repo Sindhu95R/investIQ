6c03fe45cca1c8b5c12e5654302c1db6db4f1eb7
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-12-23 08:46:39 +0300

    Added serialization

--- server/intelIQ/models.py
+++ server/intelIQ/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     likes = models.ManyToManyField(User, blank=True)
-    author = models.ForeignKey(User, on_delete=model.CASCADE, related_name='author')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
     
     def __str__(self):
         return str(self.title)
